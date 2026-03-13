948e3b78af5ade03742a452e2fc92a8ee2a69815
pratikmadavi90 <pratikmadavi11@gmail.com>
2026-03-13 21:49:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
