293bf7ab10154bffecd242c030a61b59852094f3
pratikmadavi90 <pratikmadavi11@gmail.com>
2026-03-13 21:02:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.get("/", (req, res) => {
   res.send("Harzo backend running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
