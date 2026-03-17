26b9d1944ec3ee48bb683a25909def932558847d
Pratik Madavi <pratikmadavi11@gmail.com>
2026-03-17 20:53:40 +0530

    fix model correct

--- routes/categoryroutes.js
+++ routes/categoryroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../models/category");
+const Category = require("./categoryModel");
 
 // GET all categories
 router.get("/", async (req, res) => {
